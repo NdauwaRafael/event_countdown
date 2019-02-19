9bf90c7dc39baf97f114c61cb6bbf64e340c9318
Raphael Karanja <rkaranja@cytonn.com>
2019-02-19 16:01:23 +0300

    UPDATE:  style gs gavu setInterval of one seconds which events state will update and add a variable timer

--- EventList.js
+++ EventList.js
@@ -1,27 +1,47 @@
 import React, {Component} from 'react';
-import {Text, FlatList} from 'react-native';
+import {StyleSheet, FlatList} from 'react-native';
 import EventCard from './EventCard'
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+        paddingTop: 20,
+        marginTop: 40,
+        backgroundColor: '#F3F3F3'
+    }
+})
 
 class EventList extends Component {
     state = {
         events: []
     }
 
-    componentDidMount(){
-        const events = require('./db.json').events.map(e=>({
+    componentDidMount() {
+        setInterval(()=>{
+            this.setState({
+                events: this.state.events.map(evt=>({
+                    ...evt,
+                    timer: Date.now()
+                }))
+            })
+        }, 1000)
+
+        const events = require('./db.json').events.map(e => ({
             ...e,
             date: new Date(e.date)
         }));
         this.setState({events});
     }
-    render(){
+
+    render() {
         const {events} = this.state;
         return (
             <FlatList
-            data={events}
-            renderItem={({item})=><EventCard event={item} />}
-            keyExtractor={item => item.id}
-             />
+                style={styles.list}
+                data={events}
+                renderItem={({item}) => <EventCard event={item}/>}
+                keyExtractor={item => item.id}
+            />
         )
     }
 }
