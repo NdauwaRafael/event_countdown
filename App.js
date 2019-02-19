e9c3d8d32950086652355ad7793ba599956162c4
Raphael Karanja <rkaranja@cytonn.com>
2019-02-19 17:40:22 +0300

    DEBUG: Use modern way of defining react navigation

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {YellowBox} from 'react-native';
-import {StackNavigator} from 'react-navigation';
+// import {YellowBox} from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import EventList from './EventList';
-
-YellowBox.ignoreWarnings([
-    'Warning: componentWillMount is deprecated',
-    'Warning: componentWillRecieveProps is deprecated'
-]);
-
-export default StackNavigator({
-    list: {
+//
+// YellowBox.ignoreWarnings([
+//     'Warning: componentWillMount is deprecated',
+//     'Warning: componentWillRecieveProps is deprecated'
+// ]);
+const AppNavigator = createStackNavigator({
+    Home: {
         screen: EventList
     }
-})
+});
+export default createAppContainer(AppNavigator);
 
 
