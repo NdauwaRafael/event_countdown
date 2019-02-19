53901d6a2e94cf11a5f1243b3f9fa5b2ac027ab6
Raphael Karanja <rkaranja@cytonn.com>
2019-02-19 17:12:59 +0300

    UPDATE:  add basic boilerplate for EventForm component

---
+++ EventForm.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    TouchableHighlight
+} from 'react-native';
+class EventForm extends Component{
+
+}
+export default EventForm;
