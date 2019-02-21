38b617985349e08b045e211b3209fe0df5a8034c
Raphael Karanja <rkaranja@cytonn.com>
2019-02-21 17:40:47 +0300

    UPDATE:  style fieldContainer

--- EventForm.js
+++ EventForm.js
@@ -8,7 +8,12 @@ import {
 } from 'react-native';
 
 const styles = StyleSheet.create({
-
+    fieldContainer: {
+        marginTop: 20,
+        marginBottom: 20,
+        backgroundColor: '#fff'
+    },
+    text: {}
 });
 class EventForm extends Component {
     static navigationOptions = {
@@ -23,7 +28,7 @@ class EventForm extends Component {
         return (
             <View style={{flex: 1}}>
                 <View style={styles.fieldContainer}>
-
+                    <TextInput style={styles.tetx}/>
                 </View>
                 <TouchableHighlight
                     onPress={this.handleAddPress.bind(this)}>
