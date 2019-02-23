76379f7a15a525bbb73295687069b037acaf2064
Raphael Karanja <rkaranja@cytonn.com>
2019-02-23 19:04:50 +0300

    UPDATE:  add boiler plates for create user component

--- App/Auth/Register/index.js
+++ App/Auth/Register/index.js
@@ -1,3 +1,8 @@
 /**
  * Created by Raphael Karanja on 2019-02-23.
  */
+import React, {Component} from 'react';
+import {Container, Content, Header, Form, Input, Item , Button, Label} from 'native-base';
+export default class Register extends Component {
+    
+}
