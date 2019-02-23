d437820b19b8eff817437c19bcb093a07aede479
Raphael Karanja <rkaranja@cytonn.com>
2019-02-23 23:07:09 +0300

    UPDATE:  create styles object for register component

--- App/Auth/Register/index.js
+++ App/Auth/Register/index.js
@@ -2,6 +2,7 @@
  * Created by Raphael Karanja on 2019-02-23.
  */
 import React, {Component} from 'react';
+import {StyleSheet} from 'react-native'
 import {Container, Content, Header, Form, Input, Item , Button, Label} from 'native-base';
 export default class Register extends Component {
     render() {
@@ -19,3 +20,5 @@ export default class Register extends Component {
         );
     }
 }
+
+const styles =StyleSheet.create({})
