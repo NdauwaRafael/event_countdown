04114ca6f3ffe87c8dc548cc868d62aa2a0fee0b
Raphael Karanja <rkaranja@cytonn.com>
2019-02-23 23:19:43 +0300

    UPDATE:  make login page the first page

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import EventList from './App/Events/EventList';
 import EventForm from './App/Events/EventForm';
 import AuthRegister from './App/Auth/Register';
+import AuthLogin from './App/Auth/Login';
 
 const AppNavigator = createStackNavigator({
+    Login: {
+        screen: AuthLogin
+    },
     Register: {
         screen: AuthRegister
     },
