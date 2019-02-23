cec7f786f776c268f66096bfe8940aa6d0e41b88
Raphael Karanja <rkaranja@cytonn.com>
2019-02-23 19:08:24 +0300

    UPDATE:  pass basic input props

--- App/Auth/Register/index.js
+++ App/Auth/Register/index.js
@@ -10,7 +10,9 @@ export default class Register extends Component {
                 <Form>
                     <Item>
                         <Label>Email</Label>
-                        <Input/>
+                        <Input
+                        autoCorrect={false}
+                        autoCapitalize="none"/>
                     </Item>
                 </Form>
             </Container>
