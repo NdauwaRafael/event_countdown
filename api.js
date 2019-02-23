92d48a28467352bbb2e2dc8aa964593e3b85e80d
Raphael Karanja <rkaranja@cytonn.com>
2019-02-23 18:29:43 +0300

    UPDATE:  install firbase and remove getEvent method

--- api.js
+++ api.js
@@ -1,17 +1,17 @@
 import moment from 'moment';
 import Expo from 'expo';
 const {manifest} = Expo.Constants;
-const api = (typeof manifest.packagerOpts === `object`) && manifest.packagerOpts.dev ? manifest.debuggerHost.split(':').shift().concat(':3004') : 'production.com';
-const evnts_url = `http://${api}/events`;
-
-export function getEvents() {
-    return fetch(evnts_url)
-        .then(response => response.json())
-        .then(events=> events.map(e => ({
-            ...e,
-            date: new Date(e.date)
-        })));
-}
+// const api = (typeof manifest.packagerOpts === `object`) && manifest.packagerOpts.dev ? manifest.debuggerHost.split(':').shift().concat(':3004') : 'production.com';
+// const evnts_url = `http://localhost/events`;
+//
+// export function getEvents() {
+//     return fetch(evnts_url)
+//         .then(response => response.json())
+//         .then(events=> events.map(e => ({
+//             ...e,
+//             date: new Date(e.date)
+//         })));
+// }
 export function formatDateTime(dateString) {
     const parsed = moment(new Date(dateString));
     if (!parsed.isValid()) {
