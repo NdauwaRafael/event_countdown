39b479e6dd5f164ac62ae345286bccd9070a064e
Raphael Karanja <rkaranja@cytonn.com>
2019-02-23 20:39:11 +0300

    UPDATE:  add navigation to register page

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import EventList from './App/Events/EventList';
 import EventForm from './App/Events/EventForm';
+import AuthRegister from './App/Auth/Register';
 
 const AppNavigator = createStackNavigator({
+    Register: {
+        screen: AuthRegister
+    },
     List: {
         screen: EventList
     },
