28c2b298ba07e200f08715ccdf0b6f26eed8f8ae
Raphael Karanja <rkaranja@cytonn.com>
2019-02-23 18:35:57 +0300

    UPDATE:  add firebasic basic configuration boiler plate

--- api.js
+++ api.js
@@ -1,17 +1,11 @@
 import moment from 'moment';
-// import Expo from 'expo';
-// const {manifest} = Expo.Constants;
-// const api = (typeof manifest.packagerOpts === `object`) && manifest.packagerOpts.dev ? manifest.debuggerHost.split(':').shift().concat(':3004') : 'production.com';
-// const evnts_url = `http://localhost/events`;
-//
-// export function getEvents() {
-//     return fetch(evnts_url)
-//         .then(response => response.json())
-//         .then(events=> events.map(e => ({
-//             ...e,
-//             date: new Date(e.date)
-//         })));
-// }
+import * as Firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "<YOUR-API-KEY>",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: ""
+}
 export function formatDateTime(dateString) {
     const parsed = moment(new Date(dateString));
     if (!parsed.isValid()) {
