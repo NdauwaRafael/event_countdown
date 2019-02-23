0e82980f497f296caa2db761d0062342604280f8
Raphael Karanja <rkaranja@cytonn.com>
2019-02-23 23:18:56 +0300

    UPDATE:  add login page

--- App/Auth/Login/index.js
+++ App/Auth/Login/index.js
@@ -1,3 +1,47 @@
 /**
  * Created by Raphael Karanja on 2019-02-23.
  */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text} from 'react-native'
+import {Container, Content, Header, Form, Input, Item, Button, Label} from 'native-base';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <Container style={styles.container}>
+                <Form>
+                    <Item floatingLabel>
+                        <Label>Email</Label>
+                        <Input
+                            autoCorrect={false}
+                            autoCapitalize="none"/>
+                    </Item>
+
+                    <Item floatingLabel>
+                        <Label>Password</Label>
+                        <Input
+                            secureTextEntry={true}
+                            autoCorrect={false}
+                            autoCapitalize="none"/>
+                    </Item>
+
+                    <Button
+                        full
+                        rounded
+                        success>
+                        <Text>Login</Text>
+                    </Button>
+                </Form>
+            </Container>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: "center"
+    }
+})
