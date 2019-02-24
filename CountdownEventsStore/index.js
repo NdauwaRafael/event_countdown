40a1a06cf08f96d55fdb1736c85d55ee37a3b369
Raphael Karanja <rkaranja@cytonn.com>
2019-02-24 23:42:32 +0300

    UPDATE:  add store main configuration file

--- CountdownEventsStore/index.js
+++ CountdownEventsStore/index.js
@@ -1,3 +1,15 @@
 /**
  * Created by Raphael Karanja on 2019-02-24.
  */
+import rootReducer from './RootReducer';
+import {createStore, applyMiddleware} from "redux";
+import reduxImmutableStatateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
+const configureStore = (initialState)=>{
+    return createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(thunk, reduxImmutableStatateInvariant())
+    );
+};
+export default configureStore;
