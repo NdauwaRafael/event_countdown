ff57a414cc6d5fc02ee38f7496a60f15f5339cd9
Raphael Karanja <rkaranja@cytonn.com>
2019-02-25 23:05:59 +0300

    UPDATE:  add expo fire base instance

--- src/Utils/Auth/Firebase/index.js
+++ src/Utils/Auth/Firebase/index.js
@@ -1,10 +1,11 @@
 /**
  * Created by Raphael Karanja on 2019-02-25.
  */
-import * as firebase from "firebase";
-
-import { FirebaseConfig } from "../FirebaseKeys.js";
-firebase.initializeApp(FirebaseConfig);
+// import * as firebase from "firebase";
+//
+// import { FirebaseConfig } from "../FireBaseConfig.js";
+// firebase.initializeApp(FirebaseConfig);
+import firebase from 'expo-firebase-app';
 
 const databaseRef = firebase.database().ref();
 export const eventsRef = databaseRef.child("events");
