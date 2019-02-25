e2f8532b8eb58584410c9317fb0d1979360947cd
Raphael Karanja <rkaranja@cytonn.com>
2019-02-25 21:38:58 +0300

    UPDATE:  add const types for fetching events and fetching users

--- src/CountdownEventsStore/actions/Login/index.js
+++ src/CountdownEventsStore/actions/Login/index.js
@@ -1,3 +1,5 @@
 /**
  * Created by Raphael Karanja on 2019-02-25.
  */
+import { todosRef, authRef, provider } from "../../../Utils/Auth/Firebase";
+import { FETCH_EVENTS_SUCCESS, FETCH_USER_SUCCESS } from "../../constants/ActionTypes";
