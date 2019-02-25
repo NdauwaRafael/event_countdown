ceeda841c9e6d49f7e9112aa3aa66354f82c86a3
Raphael Karanja <rkaranja@cytonn.com>
2019-02-25 21:55:36 +0300

    UPDATE:  add fetch user reducer

--- src/CountdownEventsStore/reducers/Login/index.js
+++ src/CountdownEventsStore/reducers/Login/index.js
@@ -1,3 +1,12 @@
 /**
  * Created by Raphael Karanja on 2019-02-25.
  */
+import {FETCH_USER_SUCCESS} from '../../constants/ActionTypes'
+export default (state = {}, action)=>{
+    switch (action.type)  {
+        case FETCH_USER_SUCCESS:
+            return action.user;
+        default:
+            return state;
+    }
+}
--- src/CountdownEventsStore/reducers/index.js
+++ src/CountdownEventsStore/reducers/index.js
@@ -2,5 +2,8 @@
  * Created by Raphael Karanja on 2019-02-25.
  */
 import {combineReducers} from 'redux';
-const rootReducer = combineReducers({});
+import UserReducer from './Login'
+const rootReducer = combineReducers({
+    UserReducer
+});
 export default rootReducer;
