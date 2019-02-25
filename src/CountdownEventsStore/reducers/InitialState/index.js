6938d921684b0711eef600d66ed6ee861dc77db4
Raphael Karanja <rkaranja@cytonn.com>
2019-02-25 22:16:05 +0300

    UPDATE:  add basic redux configurations for login page

--- src/CountdownEventsStore/reducers/InitialState/index.js
+++ src/CountdownEventsStore/reducers/InitialState/index.js
@@ -2,8 +2,10 @@
  * Created by Raphael Karanja on 2019-02-24.
  */
 export default {
-    loggingIn: false,
-    loggedIn: false,
-    signingIn: false,
-    loggingOut: false,
+    auth: {
+        loggingIn: false,
+        loggedIn: false,
+        signingIn: false,
+        loggingOut: false,
+    }
 }
--- src/CountdownEventsStore/reducers/Login/index.js
+++ src/CountdownEventsStore/reducers/Login/index.js
@@ -1,11 +1,28 @@
 /**
  * Created by Raphael Karanja on 2019-02-25.
  */
-import {FETCH_USER_SUCCESS} from '../../constants/ActionTypes'
-export default (state = {}, action)=>{
-    switch (action.type)  {
-        case FETCH_USER_SUCCESS:
-            return action.user;
+import {
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAILED,
+    CREATE_USER_SUCCESS,
+    CREATE_USER_FAIL,
+    LOG_OUT
+} from '../../constants/ActionTypes';
+import initialState from '../InitialState';
+
+export default (state = initialState.auth, action) => {
+    switch (action.type) {
+        case LOGIN_USER_SUCCESS:
+            const userId = action.user.uid;
+            return {...state, loggedIn: true, userId};
+        case(LOGIN_USER_FAILED):
+            return {...state, loggedIn: false};
+        case(LOG_OUT):
+            return {loggingOut: true};
+        case(CREATE_USER_SUCCESS):
+            return {...state};
+        case(CREATE_USER_FAIL):
+            return {...state};
         default:
             return state;
     }
