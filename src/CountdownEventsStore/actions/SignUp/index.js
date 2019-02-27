aa7dad45826e16c872aafc6809ab645b637eb297
Raphael Karanja <rkaranja@cytonn.com>
2019-02-27 21:44:30 +0300

    UPDATE:  add create user method

--- src/CountdownEventsStore/actions/SignUp/index.js
+++ src/CountdownEventsStore/actions/SignUp/index.js
@@ -1,6 +1,6 @@
 /**
  * Created by Raphael Karanja on 2019-02-27.
- */   
+ */
 import {authRef, provider } from "../../../Utils/Auth/Firebase";
 import {CREATE_USER_SUCCESS, CREATE_USER_FAIL } from "../../constants/ActionTypes";
 export const createUserSuccess = (resp) => {
@@ -16,5 +16,9 @@ export const createUserFail = (error) => {
         type: CREATE_USER_FAIL,
         error
     }
+};
+
+export const createUser = ()=>dispatch=>{
+    
 }
 
