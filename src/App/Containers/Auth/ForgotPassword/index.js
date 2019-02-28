a8b1f1a0204cdd0a438fe55d405388de25942f81
Raphael Karanja <rkaranja@cytonn.com>
2019-02-28 23:29:39 +0300

    UPDATE:  add basic component boiler plate to forgot password component

--- src/App/Containers/Auth/ForgotPassword/index.js
+++ src/App/Containers/Auth/ForgotPassword/index.js
@@ -1,3 +1,12 @@
 /**
  * Created by Raphael Karanja on 2019-02-28.
  */
+import React, {Component} from 'react';
+
+class ForgotPassword extends Component{
+    constructor(props){
+        super(props)
+    }
+}
+
+export default ForgotPassword;
