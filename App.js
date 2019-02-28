2370ad9351a896c8c5028cf7958c195fe12cd1ea
Raphael Karanja <rkaranja@cytonn.com>
2019-02-28 23:17:54 +0300

    DEBUG: Cleanup unused imports in app.js

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 import configureStore from './src/CountdownEventsStore';
 import {checkAuthState} from './src/CountdownEventsStore/actions/Login';
 import {Provider} from 'react-redux';
 import Navigator from './src/App/Components/Navigator/AppNavigator';
-
-
 const store = configureStore();
 store.dispatch(checkAuthState);
-
-
 
 export default class App extends React.Component {
 
