6da6b9a454bee44cc3afa465cc57e841e99da544
Raphael Karanja <rkaranja@cytonn.com>
2019-02-28 23:17:31 +0300

    DEBUG: Cleanup unused imports in app.js

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
 import React, {Component} from 'react';
-import {createStackNavigator, createAppContainer} from 'react-navigation';
-import EventList from './src/App/Containers/Events/EventList';
-import EventForm from './src/App/Containers/Events/EventForm';
-import AuthRegister from './src/App/Containers/Auth/Register';
-import AuthLogin from './src/App/Containers/Auth/Login';
-import Loading from './src/App/Containers/Auth/Loading';
 import configureStore from './src/CountdownEventsStore';
 import {checkAuthState} from './src/CountdownEventsStore/actions/Login';
 import {Provider} from 'react-redux';
@@ -14,24 +8,7 @@ import Navigator from './src/App/Components/Navigator/AppNavigator';
 const store = configureStore();
 store.dispatch(checkAuthState);
 
-const AppNavigator = createStackNavigator({
-    Loading: {
-        screen: Loading
-    },
-    Login: {
-        screen: AuthLogin
-    },
-    Register: {
-        screen: AuthRegister
-    },
-    List: {
-        screen: EventList
-    },
-    Form: {
-        screen: EventForm
-    }
-});
-// const AppComponent = createAppContainer(AppNavigator);
+
 
 export default class App extends React.Component {
 
