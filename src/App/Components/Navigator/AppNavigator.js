9b2e768293d86a732d6ad94c10674cfe8d3d521b
Raphael Karanja <rkaranja@cytonn.com>
2019-03-01 12:06:45 +0300

    UPDATE:  add tab navigator for auth pages

--- src/App/Components/Navigator/AppNavigator.js
+++ src/App/Components/Navigator/AppNavigator.js
@@ -2,18 +2,51 @@
  * Created by Raphael Karanja on 2019-02-28.
  */
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 import AuthLogin from "../../Containers/Auth/Login";
 import AuthRegister from "../../Containers/Auth/Register";
 import EventForm from "../../Containers/Events/EventForm";
+import CustomHeader from '../CustomHeader'
 
-export default createAppContainer(createSwitchNavigator({
-    Login: AuthLogin,
-    Register: AuthRegister,
-    Main: MainTabNavigator,
-    Form: EventForm
-},{
-    backBehavior: 'none',
-}));
+export default createAppContainer(createStackNavigator({
+        Login: {
+            screen: AuthLogin,
+            navigationOptions: {
+                headerTitle: 'Login'
+            }
+        },
+
+        Register: {
+            screen: AuthRegister,
+            navigationOptions: {
+                headerTitle: "Sign Up"
+            }
+        },
+        Main: MainTabNavigator,
+        Form: {
+            screen: EventForm,
+            navigationOptions: {
+                headerTitle: "Create Event"
+            }
+        }
+    },
+    {
+        initialRouteName: "Login",
+        navigationOptions: {
+            header: props => <CustomHeader {...props} />,
+            headerStyle: {
+                backgroundColor: "transparent"
+            },
+            headerTitleStyle: {
+                fontWeight: "bold",
+                color: "#fff",
+                zIndex: 1,
+                fontSize: 18,
+                lineHeight: 23
+            },
+            headerTintColor: "#fff",
+            animationEnabled: true
+        }
+    }));
