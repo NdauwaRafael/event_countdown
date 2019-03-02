af21aae5a1bd8fa03a627f4ff1ac6082fbbb7201
Raphael Karanja <rkaranja@cytonn.com>
2019-03-03 00:29:12 +0300

    update appjs route

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import configureStore from "./src/CountdownEventsStore";
-const store = configureStore();
 import AppRoot from './src/App/index';
-
 
 export default class App extends React.Component {
     render() {
         return (
-            <Provider store={store}>
-                <AppRoot/>
-            </Provider>
+            <AppRoot/>
         );
     }
 }
