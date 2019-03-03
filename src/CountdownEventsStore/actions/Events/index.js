3e8cf8b43a54f9486504a2fa5963d337ceb7ef07
Raphael Karanja <rkaranja@cytonn.com>
2019-03-03 10:55:51 +0300

    UPDATE:  add actions and reducer files forfetching events

--- src/CountdownEventsStore/actions/Events/index.js
+++ src/CountdownEventsStore/actions/Events/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by Raphael Karanja on 2019-03-03.
  */
-import {authRef } from "../../../Utils/Auth/Firebase";
+// import {authRef } from "../../../Utils/Auth/Firebase";
 import {FETCH_EVENTS_SUCCESS } from "../../constants/ActionTypes";
 
 export const fetchEventsSuccess = (resp) =>{
