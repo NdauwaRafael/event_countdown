4b5f041e1e982b6779fe22968ed38e83624305b7
Raphael Karanja <rkaranja@cytonn.com>
2019-03-03 10:58:31 +0300

    UPDATE:  make initial state to be an array

--- src/CountdownEventsStore/reducers/Events/index.js
+++ src/CountdownEventsStore/reducers/Events/index.js
@@ -2,14 +2,13 @@
  * Created by Raphael Karanja on 2019-03-03.
  */
 import {FETCH_EVENTS_SUCCESS} from '../../constants/ActionTypes';
-let initialState = {
-    events: []
-}
+
+let initialState = [];
 export default (state = initialState, action) => {
     switch (action.type) {
         case FETCH_EVENTS_SUCCESS :
-                let events = action.events;
-                return [...events];
+            let events = action.events;
+            return [...events];
             break;
         default :
             return state;
