6f4dc70e2617606edfe0e92a5a188eb368a79340
Raphael Karanja <rkaranja@cytonn.com>
2019-03-03 10:49:58 +0300

    UPDATE:  add fetch events action functions

--- src/CountdownEventsStore/actions/Events/index.js
+++ src/CountdownEventsStore/actions/Events/index.js
@@ -3,3 +3,19 @@
  */
 import {authRef } from "../../../Utils/Auth/Firebase";
 import {FETCH_EVENTS_SUCCESS } from "../../constants/ActionTypes";
+
+export const fetchEventsSuccess = (resp) =>{
+    return {
+        type: FETCH_EVENTS_SUCCESS,
+        events: resp
+    }
+}
+
+export const fetchEvents = ()=>dispatch=>{
+    const events = require('../../../../db.json').events.map(e => ({
+        ...e,
+        date: new Date(e.date)
+    }));
+
+    return dispatch(fetchEventsSuccess(events));
+}
